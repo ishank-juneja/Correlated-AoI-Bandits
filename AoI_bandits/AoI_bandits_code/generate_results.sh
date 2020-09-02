7a5ddf3652c2d4056de71201da8a47461a2975d4
ishank-juneja <ishankjuneja@gmail.com>
2020-09-02 23:57:47 +0530

    Multiple reference C-Bandit added
    Former-commit-id: 509c8cbbcd37a25a0af4516c3b8118881c9a8d61 [formerly bc658c1f1af40b82d4545d6d5605234e2369993e]
    Former-commit-id: adab448f0c61394bb506a531f7197a6151353008
    Former-commit-id: d16ee8dbf5f7394bdc76f2c3d51c1a68024d4612
    Former-commit-id: c85039750a8608dc4b77c4b9f9e6b086e8b3516c

--- AoI_bandits/AoI_bandits_code/generate_results.sh
+++ AoI_bandits/AoI_bandits_code/generate_results.sh
@@ -7,14 +7,14 @@ for instance in instances/*.txt; do
 	echo -e $"Visualizing instance functions $instance\n"
 	python3 instance_plotter.py -i "$instance"
 	# Slow step so check if output file already exists
-	out_name="results/$(basename "$instance" .txt)-out-ucb-cucb.txt"
+	out_name="results/$(basename "$instance" .txt)-out.txt"
 	# echo $out_name	
 	if [ -f "$out_name" ]; then
 		echo -e $"Results for instance $instance exist, ... skipping simulation\n"
 	else
-		echo -e $"Currently Simulating Policies on $instance"
-		python3 simulate_policies.py -i "$instance" > "$out_name"
-	fi	
+	echo -e $"Currently Simulating Policies on $instance"
+	python3 simulate_policies.py -i "$instance" > "$out_name"
+	fi
 	echo -e $"Plotting Simulation results for $instance \n"
 	python3 regret_plotter.py -i "$out_name"
 done
